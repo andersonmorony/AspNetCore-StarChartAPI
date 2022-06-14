07658ef3a48aae1432ab354513d8deab2b0d9c06
andersonmorony <andersonmoroni92@gmail.com>
2022-06-14 06:12:42 -0300

    feat: completed module create Methods

--- StarChart/Controllers/CelestialObjectController.cs
+++ StarChart/Controllers/CelestialObjectController.cs
@@ -17,5 +17,32 @@ namespace StarChart.Controllers
         {
             _context = context;
         }
+
+        [HttpGet("{id:int}")]
+        public IActionResult GetById(int Id)
+        {
+            var result = _context.CelestialObjects.FirstOrDefault(e => e.OrbitedObjectId == Id);
+
+            if(result != null)
+            {
+                return Ok(result);
+            }
+            return NotFound();
+        }
+        [HttpGet("name")]
+        public IActionResult GetByName(string name)
+        {
+            var result = _context.CelestialObjects.FirstOrDefault(e => e.Name == name);
+            if (result != null)
+            {
+                return Ok(result);
+            }
+            return NotFound();
+        }
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            return Ok(_context.CelestialObjects.ToList());
+        }
     }
 }
